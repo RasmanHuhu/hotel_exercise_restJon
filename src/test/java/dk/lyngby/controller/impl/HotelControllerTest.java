85fe9874343d4f8678821b21d80774b0567ad4f5
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-10-12 10:51:08 +0200

    jaja

--- src/test/java/dk/lyngby/controller/impl/HotelControllerTest.java
+++ src/test/java/dk/lyngby/controller/impl/HotelControllerTest.java
@@ -217,12 +217,12 @@ class HotelControllerTest
     @NotNull
     private static Set<Room> getCalRooms()
     {
-        Room r100 = new Room(100, new BigDecimal(2520), Room.RoomType.SINGLE);
-        Room r101 = new Room(101, new BigDecimal(2520), Room.RoomType.SINGLE);
-        Room r102 = new Room(102, new BigDecimal(2520), Room.RoomType.SINGLE);
-        Room r103 = new Room(103, new BigDecimal(2520), Room.RoomType.SINGLE);
-        Room r104 = new Room(104, new BigDecimal(3200), Room.RoomType.DOUBLE);
-        Room r105 = new Room(105, new BigDecimal(4500), Room.RoomType.SUITE);
+        Room r100 = new Room(100, new BigDecimal(2520), Room.RoomType.SINGLE, 50);
+        Room r101 = new Room(101, new BigDecimal(2520), Room.RoomType.SINGLE, 50);
+        Room r102 = new Room(102, new BigDecimal(2520), Room.RoomType.SINGLE, 50);
+        Room r103 = new Room(103, new BigDecimal(2520), Room.RoomType.SINGLE, 50);
+        Room r104 = new Room(104, new BigDecimal(3200), Room.RoomType.DOUBLE, 100);
+        Room r105 = new Room(105, new BigDecimal(4500), Room.RoomType.SUITE, 150);
 
         Room[] roomArray = {r100, r101, r102, r103, r104, r105};
         return Set.of(roomArray);
@@ -231,12 +231,12 @@ class HotelControllerTest
     @NotNull
     private static Set<Room> getBatesRooms()
     {
-        Room r111 = new Room(111, new BigDecimal(2520), Room.RoomType.SINGLE);
-        Room r112 = new Room(112, new BigDecimal(2520), Room.RoomType.SINGLE);
-        Room r113 = new Room(113, new BigDecimal(2520), Room.RoomType.SINGLE);
-        Room r114 = new Room(114, new BigDecimal(2520), Room.RoomType.DOUBLE);
-        Room r115 = new Room(115, new BigDecimal(3200), Room.RoomType.DOUBLE);
-        Room r116 = new Room(116, new BigDecimal(4500), Room.RoomType.SUITE);
+        Room r111 = new Room(111, new BigDecimal(2520), Room.RoomType.SINGLE, 50);
+        Room r112 = new Room(112, new BigDecimal(2520), Room.RoomType.SINGLE, 50);
+        Room r113 = new Room(113, new BigDecimal(2520), Room.RoomType.SINGLE, 50);
+        Room r114 = new Room(114, new BigDecimal(2520), Room.RoomType.DOUBLE, 100);
+        Room r115 = new Room(115, new BigDecimal(3200), Room.RoomType.DOUBLE, 100);
+        Room r116 = new Room(116, new BigDecimal(4500), Room.RoomType.SUITE, 150);
 
         Room[] roomArray = {r111, r112, r113, r114, r115, r116};
         return Set.of(roomArray);
